3054af4e6e7f4df53dd186fb6c90927cc75ee513
INHO LEE <zergling2565@gmail.com>
2025-10-22 15:15:53 +0900

    2025-10-22

---
+++ index.js
@@ -0,0 +1,18 @@
+// --------------------------------
+// 01
+// --------------------------------
+
+// const _ = require("lodash")
+
+// const arr = [1, 2, 3, 4, 5];
+// const shuffled = _.shuffle(arr);
+// console.log("Shuffled:", shuffled);
+// console.log("Hello World");
+
+// -----------------------------------
+// 02
+// -----------------------------------
+import _ from 'lodash'
+const arr = [1, 2, 3, 4, 5];
+const shuffled = _.shuffle(arr);
+console.log("shuffled_!!!!", shuffled);
